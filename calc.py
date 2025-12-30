3a90966dcc77f25ca406656524690343e4e1c830
sravya <sravyakatikala18@gmail.com>
2025-12-30 21:42:17 +0530

    calculator code in main branch by user1

---
+++ calc.py
@@ -0,0 +1,16 @@
+^[[200~# simple calculator project
+
+def add(x,y):
+  return x + y
+
+def subtract(x,y):
+  pass # to be implemented
+
+def multipy(x,y):
+  pass # to be implemented
+
+def divide(x,y):
+  pass # to be implemented
+
+if __name__==__main__:
+  print(calculator started)~
